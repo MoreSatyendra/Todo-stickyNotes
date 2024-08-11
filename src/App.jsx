4c937db416c16bf00c514c6bdd81b6e278108300
MoreSatyendra <satyendramore97@gmail.com>
2024-08-11 12:49:08 +0530

    newChange update

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import NotesPage from "./pages/NotesPage";
+
+function App() {
+  return (
+    <div id="app">
+      <NotesPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/NoteCard.jsx
@@ -0,0 +1,75 @@
+import React, { useEffect, useRef, useState } from "react";
+import Trash from "../icons/Trash";
+import { autoGrow, setNewOffset } from "../utils";
+
+const NoteCard = ({ note }) => {
+  const [position, setPosition] = useState(JSON.parse(note.position));
+  const colors = JSON.parse(note.colors);
+  const body = JSON.parse(note.body);
+
+  let mouseStartPos = { x: 0, y: 0 };
+
+  const cardRef = useRef(null);
+  const textAreaRef = useRef(null);
+
+  const mouseDown = (e) => {
+    mouseStartPos.x = e.clientX;
+    mouseStartPos.y = e.clientY;
+
+    document.addEventListener("mousemove", mouseMove);
+    document.addEventListener("mouseup", mouseUp);
+  };
+
+  const mouseMove = (e) => {
+    let mouseMoveDir = {
+      x: mouseStartPos.x - e.clientX,
+      y: mouseStartPos.y - e.clientY,
+    };
+    mouseStartPos.x = e.clientX;
+    mouseStartPos.y = e.clientY;
+
+    const newPosition = setNewOffset(cardRef.current, mouseMoveDir);
+    setPosition(newPosition);
+  };
+
+  const mouseUp = (e) => {
+    document.removeEventListener("mousemove", mouseMove);
+    document.removeEventListener("mouseup", mouseUp);
+  };
+
+  useEffect(() => {
+    autoGrow(textAreaRef);
+  }, []);
+
+  return (
+    <div
+      ref={cardRef}
+      className="card"
+      style={{
+        backgroundColor: colors.colorBody,
+        left: `${position.x}px`,
+        top: `${position.y}px`,
+      }}
+    >
+      <div
+        className="card-header"
+        style={{
+          backgroundColor: colors.colorHeader,
+        }}
+        onMouseDown={mouseDown}
+      >
+        <Trash />
+      </div>
+      <div className="card-body">
+        <textarea
+          ref={textAreaRef}
+          defaultValue={body}
+          onInput={() => autoGrow(textAreaRef)}
+          style={{ color: colors.colorText }}
+        ></textarea>
+      </div>
+    </div>
+  );
+};
+
+export default NoteCard;
