c91ae88bce9db60719f54f6a3aaecc704dc80070
MoreSatyendra <satyendramore97@gmail.com>
2024-08-11 13:36:38 +0530

    created function performed on database

--- src/pages/NotesPage.jsx
+++ src/pages/NotesPage.jsx
@@ -1,16 +1,13 @@
 import React, { useEffect, useState } from "react";
 import NoteCard from "../components/NoteCard";
 import { databases } from "../appwrite/config";
+import { db } from "../appwrite/database";
 
 const NotesPage = () => {
   const [notes, setNotes] = useState([]);
 
   const init = async () => {
-    const response = await databases.listDocuments(
-      import.meta.env.VITE_DATABASE_ID,
-      import.meta.env.VITE_COLLECTION_NOTES_ID
-    );
-
+    const response = await db.notes.list();
     setNotes(response.documents);
   };
 
