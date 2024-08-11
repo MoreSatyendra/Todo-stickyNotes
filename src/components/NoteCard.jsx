927b02e9d9b2c522809966e45e292c084eaa8982
MoreSatyendra <satyendramore97@gmail.com>
2024-08-11 12:56:00 +0530

    added onFocus function on card

--- src/components/NoteCard.jsx
+++ src/components/NoteCard.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef, useState } from "react";
 import Trash from "../icons/Trash";
-import { autoGrow, setNewOffset } from "../utils";
+import { autoGrow, setNewOffset, setZIndex } from "../utils";
 
 const NoteCard = ({ note }) => {
   const [position, setPosition] = useState(JSON.parse(note.position));
@@ -13,6 +13,7 @@ const NoteCard = ({ note }) => {
   const textAreaRef = useRef(null);
 
   const mouseDown = (e) => {
+    setZIndex(cardRef.current);
     mouseStartPos.x = e.clientX;
     mouseStartPos.y = e.clientY;
 
@@ -64,8 +65,9 @@ const NoteCard = ({ note }) => {
         <textarea
           ref={textAreaRef}
           defaultValue={body}
-          onInput={() => autoGrow(textAreaRef)}
           style={{ color: colors.colorText }}
+          onInput={() => autoGrow(textAreaRef)}
+          onFocus={() => setZIndex(cardRef.current)}
         ></textarea>
       </div>
     </div>
